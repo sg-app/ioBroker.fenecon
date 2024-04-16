d54134207e9c02557502006a91e5cc848645da14
Georg Schreiner <info@sg-app.de>
2024-04-16 18:29:39 +0000

    first version

--- main.js
+++ main.js
@@ -9,7 +9,8 @@
 const utils = require("@iobroker/adapter-core");
 
 // Load your modules here, e.g.:
-// const fs = require("fs");
+const axios = require("axios");
+const dp = require("./lib/datapoints");
 
 class Fenecon extends utils.Adapter {
 	/**
@@ -22,8 +23,6 @@ class Fenecon extends utils.Adapter {
 		});
 		this.on("ready", this.onReady.bind(this));
 		this.on("stateChange", this.onStateChange.bind(this));
-		// this.on("objectChange", this.onObjectChange.bind(this));
-		// this.on("message", this.onMessage.bind(this));
 		this.on("unload", this.onUnload.bind(this));
 	}
 
@@ -33,55 +32,97 @@ class Fenecon extends utils.Adapter {
 	async onReady() {
 		// Initialize your adapter here
 
-		// The adapters config (in the instance object everything under the attribute "native") is accessible via
-		// this.config:
-		this.log.info("config option1: " + this.config.option1);
-		this.log.info("config option2: " + this.config.option2);
+		try {
 
-		/*
-		For every state in the system there has to be also an object of type state
-		Here a simple template for a boolean variable named "testVariable"
-		Because every adapter instance uses its own unique namespace variable names can't collide with other adapters variables
-		*/
-		await this.setObjectNotExistsAsync("testVariable", {
-			type: "state",
-			common: {
-				name: "testVariable",
-				type: "boolean",
-				role: "indicator",
-				read: true,
-				write: true,
-			},
-			native: {},
-		});
+			// The adapters config (in the instance object everything under the attribute "native") is accessible via
+			if (!this.config.ipAddress) {
+				this.log.error(`FEMS IP address is empty - please check configuration of ${this.namespace}`);
+				return;
+			}
 
-		// In order to get state updates, you need to subscribe to them. The following line adds a subscription for our variable we have created above.
-		this.subscribeStates("testVariable");
-		// You can also add a subscription for multiple states. The following line watches all states starting with "lights."
-		// this.subscribeStates("lights.*");
-		// Or, if you really must, you can also watch all states. Don't do this if you don't need to. Otherwise this will cause a lot of unnecessary load on the system:
-		// this.subscribeStates("*");
+			if (!this.config.ipAddress.match("^[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}")) {
+				this.log.error(`IP address has wrong format - please check configuration of ${this.namespace}`);
+				return;
+			}
+			// http://x:user@<ipAddress>/rest/channel/
+			this.log.debug("Adapter successful started.");
+			this.apiClient = axios.create({
+				baseURL: `http://x:user@${this.config.ipAddress}/rest/channel/`,
+				timeout: 5000,
+				responseType: "json",
+				responseEncoding: "utf8"
+			});
+			this.log.debug("axios instance successful created.");
 
-		/*
-			setState examples
-			you will notice that each setState will cause the stateChange event to fire (because of above subscribeStates cmd)
-		*/
-		// the variable testVariable is set to true as command (ack=false)
-		await this.setStateAsync("testVariable", true);
+			await this.loadData();
+		}
+		catch (err) {
+			this.log.error(`Error during startup wiht message: ${err.message}`);
+		}
+	}
 
-		// same thing, but the value is flagged "ack"
-		// ack should be always set to true if the value is received from or acknowledged from the target system
-		await this.setStateAsync("testVariable", { val: true, ack: true });
+	async loadData() {
+		try {
+			this.log.debug("Load data from FEMS.");
+			if (!this.apiClient) {
+				this.log.error("Apiclient not instanced.");
+				return;
+			}
 
-		// same thing, but the state is deleted after 30s (getState will return null afterwards)
-		await this.setStateAsync("testVariable", { val: true, ack: true, expire: 30 });
+			for (const item of dp.AllParams) {
+				const response = await this.apiClient.get(item.Datapoint);
+				this.log.debug(`response ${response.status}: ${JSON.stringify(response.data)}`);
 
-		// examples for the checkPassword/checkGroup functions
-		let result = await this.checkPasswordAsync("admin", "iobroker");
-		this.log.info("check user admin pw iobroker: " + result);
+				if (response.status === 200) {
+					await this.updateState(item, response.data);
+				}
+			}
+		}
+		catch (err) {
+			this.log.error(`Error during loading data: ${err.message}`);
+		}
+		finally {
+			// Restart request load Data!
+			this.timer = setTimeout(async () => {
+				this.log.debug("Start next request.");
+				this.timer = null;
+				await this.loadData();
+			}, this.config.refreshIntervall * 1000);
+		}
+	}
+	/**
+	 * Updates state.
+	 * @param {object} item JSON Object
+	 * @param {object} data AXIOS Response Object Data
+	 */
+	async updateState(item, data) {
+		try {
 
-		result = await this.checkGroupAsync("admin", "admin");
-		this.log.info("check group user admin group admin: " + result);
+			const typeTranslation = {
+				INTEGER: "number",
+				LONG: "number"
+			};
+			this.log.silly(`ExtendObject ${item.Name} with Data: ${data.value}`);
+			await this.extendObjectAsync(item.Name,
+				{
+					common: {
+						name: item.Name,
+						desc: item.Description,
+						role: "value",
+						write: false,
+						read: true,
+						type: typeTranslation[data.type],
+						unit: data.unit,
+						states: item?.States
+					},
+					type: "state",
+					native: {}
+				});
+			await this.setStateAsync(item.Name, { val: data.value, ack: true });
+
+		} catch (err) {
+			this.log.error("Can't update states. " + err.message);
+		}
 	}
 
 	/**
@@ -90,34 +131,15 @@ class Fenecon extends utils.Adapter {
 	 */
 	onUnload(callback) {
 		try {
-			// Here you must clear all timeouts or intervals that may still be active
-			// clearTimeout(timeout1);
-			// clearTimeout(timeout2);
-			// ...
-			// clearInterval(interval1);
-
+			if (this.timer) {
+				clearTimeout(this.timer);
+				this.timer = null;
+			}
 			callback();
 		} catch (e) {
 			callback();
 		}
 	}
-
-	// If you need to react to object changes, uncomment the following block and the corresponding line in the constructor.
-	// You also need to subscribe to the objects with `this.subscribeObjects`, similar to `this.subscribeStates`.
-	// /**
-	//  * Is called if a subscribed object changes
-	//  * @param {string} id
-	//  * @param {ioBroker.Object | null | undefined} obj
-	//  */
-	// onObjectChange(id, obj) {
-	// 	if (obj) {
-	// 		// The object was changed
-	// 		this.log.info(`object ${id} changed: ${JSON.stringify(obj)}`);
-	// 	} else {
-	// 		// The object was deleted
-	// 		this.log.info(`object ${id} deleted`);
-	// 	}
-	// }
 
 	/**
 	 * Is called if a subscribed state changes
@@ -134,23 +156,6 @@ class Fenecon extends utils.Adapter {
 		}
 	}
 
-	// If you need to accept messages in your adapter, uncomment the following block and the corresponding line in the constructor.
-	// /**
-	//  * Some message was sent to this instance over message box. Used by email, pushover, text2speech, ...
-	//  * Using this method requires "common.messagebox" property to be set to true in io-package.json
-	//  * @param {ioBroker.Message} obj
-	//  */
-	// onMessage(obj) {
-	// 	if (typeof obj === "object" && obj.message) {
-	// 		if (obj.command === "send") {
-	// 			// e.g. send email or pushover or whatever
-	// 			this.log.info("send command");
-
-	// 			// Send response in callback if required
-	// 			if (obj.callback) this.sendTo(obj.from, obj.command, "Message received", obj.callback);
-	// 		}
-	// 	}
-	// }
 }
 
 if (require.main !== module) {
